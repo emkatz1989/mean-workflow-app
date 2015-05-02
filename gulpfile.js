e480961067721670ab07884bdc54a11060bf4a19
Eric Katz <emkatz1989@gmail.com>
2015-05-02 16:10:11 -0700

    gulp tasks js scripts angular

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 //load the plugins
-var gulp      = require('gulp');
-var less      = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
-var rename    = require('gulp-rename');
-var jshint    = require('gulp-jshint');
+var gulp       = require('gulp');
+var less       = require('gulp-less');
+var minifyCSS  = require('gulp-minify-css');
+var rename     = require('gulp-rename');
+var jshint     = require('gulp-jshint');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+
 
 //define a task called css
 gulp.task('css', function() {
@@ -22,4 +26,26 @@ gulp.task('js', function() {
 	return gulp.src(['server.js', 'public/app/*.js', 'public/app/**/*.js'])
 	  .pipe(jshint())
 	  .pipe(jshint.reporter('default'));
+});
+
+//task for scripts
+gulp.task('scripts', function(){
+	return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'))
+	.pipe(concat('all.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('public/dist'));
+});
+
+//task for minifying angular files
+
+gulp.task('angular', function(){
+	return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+	  .pipe(jshint())
+	  .pipe(jshint.reporter('default'))
+	  .pipe(ngAnnotate())
+	  .pipe(concat('app.js'))
+	  .pipe(uglify())
+	  .pipe(gulp.dest('public/dist'));
 });
