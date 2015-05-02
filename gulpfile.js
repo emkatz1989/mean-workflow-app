df4703fb4b2346a5481240de6472492c83f8c1e2
Eric Katz <emkatz1989@gmail.com>
2015-05-02 15:33:23 -0700

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+//load the plugins
+var gulp      = require('gulp');
+var less      = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var rename    = require('gulp-rename');
+var jshint    = require('gulp-jshint');
+
+//define a task called css
+gulp.task('css', function() {
+
+	//grab the less file, process the LESS, save to style.css
+	return gulp.src('public/assets/css/style.less')
+	   .pipe(less())
+	   .pipe(minifyCSS())
+	   .pipe(rename({ suffix: '.min' }))
+	   .pipe(gulp.dest('public/assets/css'));
+});
+
+//task for js files
+gulp.task('js', function() {
+
+	return gulp.src(['server.js', 'public/app/*.js', 'public/app/**/*.js'])
+	  .pipe(jshint())
+	  .pipe(jshint.reporter('default'));
+});
